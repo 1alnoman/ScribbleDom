cc68204a62e634050e4f2596efe7ce19d34b39f4
Noman1705088 <1705088@ugrad.cse.buet.ac.bd>
2023-08-23 00:32:39 +0600

    dlpfc and melanoma added

--- run_bcdc_ffpe.sh
+++ run_bcdc_ffpe.sh
@@ -4,20 +4,20 @@ config_file_mclust="configs/bcdc/bcdc_config_mclust.json"
 
 Rscript get_genex_data_from_10x_h5.R ${config_file_expert}
 
-# python st_data_to_matrix_representation_converter.py --params ${config_file_expert}
-# python st_data_to_matrix_representation_converter.py --params ${config_file_mclust}
-# echo "========================================"
-# echo "Data convertend in matrix representation"
+python st_data_to_matrix_representation_converter.py --params ${config_file_expert}
+python st_data_to_matrix_representation_converter.py --params ${config_file_mclust}
+echo "========================================"
+echo "Data convertend in matrix representation"
 
-# python scribble_dom.py --params ${config_file_expert}
-# python autoscribble_dom.py --params ${config_file_mclust}
-# echo "========================================"
-# echo "Model run complete"
+python scribble_dom.py --params ${config_file_expert}
+python autoscribble_dom.py --params ${config_file_mclust}
+echo "========================================"
+echo "Model run complete"
 
-# python best_model_estimator.py --params ${config_file_expert}
-# python best_model_estimator.py --params ${config_file_mclust}
-# echo "========================================"
-# echo "Best model evaluated with goodness score"
+python best_model_estimator.py --params ${config_file_expert}
+python best_model_estimator.py --params ${config_file_mclust}
+echo "========================================"
+echo "Best model evaluated with goodness score"
 
-# python show_results.py --params ${config_file_expert}
-# python show_results.py --params ${config_file_mclust}
+python show_results.py --params ${config_file_expert}
+python show_results.py --params ${config_file_mclust}
---
+++ run_human_dlpfc.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+config_file_expert="configs/human_dlpfc/dlpfc_config_expert.json"
+config_file_mclust="configs/human_dlpfc/dlpfc_config_mclust.json"
+
+Rscript get_genex_data_from_10x_h5.R ${config_file_expert}
+
+python st_data_to_matrix_representation_converter.py --params ${config_file_expert}
+echo "========================================"
+echo "Data convertend in matrix representation"
+python scribble_dom.py --params ${config_file_expert}
+echo "========================================"
+echo "Model run complete"
+python best_model_estimator.py --params ${config_file_expert}
+echo "========================================"
+echo "Best model evaluated with goodness score"
+python show_results.py --params ${config_file_expert}
+
+python st_data_to_matrix_representation_converter.py --params ${config_file_mclust}
+echo "========================================"
+echo "Data convertend in matrix representation"
+python autoscribble_dom.py --params ${config_file_mclust}
+echo "========================================"
+echo "Model run complete"
+python best_model_estimator.py --params ${config_file_mclust}
+echo "========================================"
+echo "Best model evaluated with goodness score"
+python show_results.py --params ${config_file_mclust}
