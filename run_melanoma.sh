8e9798c37a2547d1de734922f99a1f49087a3912
Noman1705088 <1705088@ugrad.cse.buet.ac.bd>
2023-09-30 06:17:31 +0600

    melanoma

--- run_melanoma.sh
+++ run_melanoma.sh
@@ -2,7 +2,7 @@
 config_file_expert="configs/melanoma/melanoma_config_expert.json"
 config_file_mclust="configs/melanoma/melanoma_config_mclust.json"
 
-python visium_data_to_matrix_representation_converter.py --params ${config_file_mclust}
+python st_data_to_matrix_representation_converter.py --params ${config_file_mclust}
 echo "========================================"
 echo "Data convertend in matrix representation"
 python autoscribble_dom.py --params ${config_file_mclust}
@@ -13,7 +13,7 @@ echo "========================================"
 echo "Best model evaluated with goodness score"
 python show_results.py --params ${config_file_mclust}
 
-python visium_data_to_matrix_representation_converter.py --params ${config_file_expert}
+python st_data_to_matrix_representation_converter.py --params ${config_file_expert}
 echo "========================================"
 echo "Data convertend in matrix representation"
 python scribble_dom.py --params ${config_file_expert}
